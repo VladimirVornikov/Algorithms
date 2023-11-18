81fc3186da8902f4a5954c299b6b35de38c51899
VladimirVornikov <vornikov-vladimir@mail.ru>
2023-11-18 12:45:04 +0100

    18.11

--- Homework8/Queue.js
+++ Homework8/Queue.js
@@ -4,56 +4,54 @@ class CustomQueue {
     #head = 0;
     #count = 0;
     constructor(value) {
-    const capacity = value;
-    const storage = new Array(capacity).fill(0);
-
-    this.offer = function (val) {
-        if (this.#count >= storage.length) {
+        this.capacity = value;
+        this.storage = new Array(this.capacity).fill(0);
+    }
+    offer(val) {
+        if (this.#count >= this.storage.length) {
             throw new Error("нет места");
         }
-        storage[this.#head++] = val;
+        this.storage[this.#head++] = val;
         this.#count++;
-        this.#head = this.#head % capacity;
+        this.#head = this.#head % this.capacity;
     };
 
-    this.poll = function () {
+    poll() {
         if (this.#count <= 0) {
             throw new Error("пусто");
         }
-        const element = storage[this.#tail];
-        storage[this.#tail] = 0;
+        const element = this.storage[this.#tail];
+        this.storage[this.#tail] = 0;
         this.#tail++;
-        this.#tail %= capacity;
+        this.#tail =  this.#tail % this.capacity;
         this.#count--;
         return element;
     };
-    
-    this.toString = function () {
+
+    get toString() {
         let res = "";
-        for (let i = 0; i < storage.length; i++) {
-            if (storage[i] === 0) {
+        for (let i = 0; i < this.storage.length; i++) {
+            if (this.storage[i] === 0) {
                 res += ". ";
             } else {
-                res += storage[i] + " ";
+                res += this.storage[i] + " ";
             }
         }
         return res;
-        };
-    }
+    };
 }
 
 function main() {
     const cq = new CustomQueue(5);
 
     for (let i = 0; i < 10; i++) {
-        console.log(cq.toString() + " << до добавления");
+        console.log(cq.toString + " << до добавления");
         cq.offer(i);
-        console.log(cq.toString() + " << после добавления");
+        console.log(cq.toString + " << после добавления");
         cq.poll();
-        console.log(cq.toString() + " << после извлечения");
+        console.log(cq.toString + " << после извлечения");
         console.log();
     }
 }
-
 
 main();
